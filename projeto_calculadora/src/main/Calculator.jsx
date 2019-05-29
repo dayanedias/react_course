074a3d914717208c0d56f0ac322799f667dab014
dayanedias <dayane.doc@hotmail.com>
2019-05-29 00:05:41 -0300

    calculadora pronta

--- projeto_calculadora/src/main/Calculator.jsx
+++ projeto_calculadora/src/main/Calculator.jsx
@@ -4,7 +4,17 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 export default class Calculator extends Component {
+
+    state = { ...initialState }
 
     constructor(props) {
         super (props)
@@ -15,15 +25,55 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        console.log('limpar')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            
+                } catch(e) {
+                    values[0] = this.state.values[0]
+                }
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+
+            })
+        }
     }
 
     addDigit(n) {
-        console.log(n)
+        
+        if (n === '.' && this.state.displayValue.includes('.')) {
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false})
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render() {
@@ -31,15 +81,15 @@ export default class Calculator extends Component {
         // const setOperation = op => this.setOperation
         return (
             <div className = "calculator">
-                <Display value={1000} />
-                <Button label="AC" click={this.clearMemory()} />
+                <Display value={this.state.displayValue} />
+                <Button label="AC" click={this.state.clearMemory} />
                 <Button label="+/-" click={this.setOperation}/>
                 <Button label="%" click={this.setOperation}/>
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit}/>
                 <Button label="8" click={this.addDigit}/>
                 <Button label="9" click={this.addDigit}/>
-                <Button label="X" click={this.setOperation} operation/>
+                <Button label="*" click={this.setOperation} operation/>
                 <Button label="4" click={this.addDigit}/>
                 <Button label="5" click={this.addDigit}/>
                 <Button label="6" click={this.addDigit}/>
@@ -47,7 +97,7 @@ export default class Calculator extends Component {
                 <Button label="1" click={this.addDigit}/>
                 <Button label="2" click={this.addDigit}/>
                 <Button label="3" click={this.addDigit}/>
-                <Button label="+" click={this.addDigit} operation/>
+                <Button label="+" click={this.setOperation} operation/>
                 <Button label="0" click={this.addDigit} double/>
                 <Button label="." click={this.addDigit}/>
                 <Button label="=" click={this.setOperation} operation/>
